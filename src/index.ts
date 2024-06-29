64709ab48e2018d33026922ddf1ae5b22a1af33a
Joel-Adving <joel.adving@gmail.com>
2024-06-30 01:34:23 +0200

    uncomment

--- src/index.ts
+++ src/index.ts
@@ -11,19 +11,19 @@ import { App } from './app'
 const rootDir = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../../')
 
 new App()
-  // .use(middleware)
-  // .get('/*', serveStatic('public'))
+  .use(middleware)
+  .get('/*', serveStatic('public'))
 
   .get('/stress-test', (req, res) => {
-    res.send('Stress test')
+    res.send('Hello world')
   })
 
-  // .get('/file', (req, res) => {
-  //   res.sendFile(path.resolve(rootDir, 'public/image.webp'))
-  // })
+  .get('/file', (req, res) => {
+    res.sendFile(path.resolve(rootDir, 'public/image.webp'))
+  })
 
-  // .group('/todos', todosHandler)
-  // .group('/users', usersHandler)
+  .group('/todos', todosHandler)
+  .group('/users', usersHandler)
 
   .listen(env.port, () => {
     console.log(`Listening to port ${env.port}`)
