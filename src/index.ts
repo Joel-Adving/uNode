160cadec8dd63bc4ccbf44bf1c8f62e01b36ec1b
Joel-Adving <joel.adving@gmail.com>
2024-06-30 14:23:09 +0200

    npm package setup

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
 import { serveStatic, getFileStats, sendFile } from './utils/file'
 import { App, Router } from './app'
 import { getIpAddress } from './utils/networking'
-import { type Middleware, type HttpMethod, type ILogger, type Request, type Response, type IApp } from './types'
+import { type Middleware, type ILogger, type Request, type Response, type IApp } from './types'
 
 export default App
 
-export {
-  Router,
-  serveStatic,
-  getFileStats,
-  sendFile,
-  getIpAddress,
-  Middleware,
-  HttpMethod,
-  ILogger,
-  Request,
-  Response,
-  IApp
-}
+export { App, serveStatic, getFileStats, sendFile, getIpAddress, Router, IApp, ILogger, Middleware, Request, Response }
